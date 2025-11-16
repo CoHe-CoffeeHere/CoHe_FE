6a8e3cda866af343723bc18b34a7c5b5aa4f9115
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-11-16 23:54:20 +0900

    feat: 홈 화면 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import CouponAlarm from "../components/CouponAlarm";
 import homeStyle from "../css/pages/home.module.css";
 import characterIcon from "../assets/pages/home/character.svg";
+import barcode from "../assets/pages/home/barcode.svg";
 
 export default function Home() {
   return (
@@ -16,6 +17,40 @@ export default function Home() {
           src={characterIcon}
           alt="Character Icon"
         />
+      </div>
+      <div className={homeStyle.subscribeCard}>
+        <div className={homeStyle.overlay}>
+          <div className={homeStyle.subscribeText}>
+            <div className={homeStyle.leftText}>
+              <span className={homeStyle.passName}>Study Pass</span>
+              <span className={homeStyle.passInfo}>월간 정기 구독</span>
+            </div>
+            <div className={homeStyle.tag}>VIP</div>
+          </div>
+          <div className={homeStyle.cntBox}>
+            <div className={homeStyle.leftCnt}>
+              <span className={homeStyle.cntTitle}>잔여 횟수</span>
+              <span className={homeStyle.cntNumber}>
+                9<span>회</span>
+              </span>
+            </div>
+            <div className={homeStyle.leftDay}>
+              <span className={homeStyle.cntTitle}>남은 기간</span>
+              <span className={homeStyle.cntNumber}>
+                15<span>일</span>
+              </span>
+            </div>
+          </div>
+          <div className={homeStyle.barcode}>
+            <img src={barcode} alt="barcode" />
+          </div>
+          <div className={homeStyle.code}>
+            <span>COHEE-PAH-2025-1114</span>
+          </div>
+          <div className={homeStyle.goToPass}>
+            코히 패스가 6일밖에 남지 않았어요 →
+          </div>
+        </div>
       </div>
     </>
   );
