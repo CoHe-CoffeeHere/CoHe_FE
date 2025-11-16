454a35e88898379579a413ea1e8e4b115cad7da1
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-11-16 23:02:32 +0900

    feat: home 상단 텍스트 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
 import CouponAlarm from "../components/CouponAlarm";
+import homeStyle from "../css/pages/home.module.css";
+import characterIcon from "../assets/pages/home/character.svg";
 
 export default function Home() {
   return (
     <>
       <CouponAlarm />
+      <div className={homeStyle.topTextDiv}>
+        <span className={homeStyle.topName}>안녕하세요, 길동님!</span>
+        <span className={homeStyle.topComment}>
+          오늘도 열정 넘치는 하루 보내세요
+        </span>
+        <img
+          className={homeStyle.characterIcon}
+          src={characterIcon}
+          alt="Character Icon"
+        />
+      </div>
     </>
   );
 }
