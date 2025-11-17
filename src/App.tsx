e5f4b510b8c7046b88c9554de139dbc840a8fa39
nyeonseok <aragorn2358@naver.com>
2025-11-17 09:16:39 +0900

    refact: Route경로 수정 및 레이아웃 병합

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import GlobalLayout from "./layout/GlobalLayout";
 import Home from "./pages/Home";
-import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<GlobalLayout />}>
-        <Route index element={<Navigate to="/main/home" replace />} />
-        <Route path="main" element={<MainLayout />}>
-          <Route path="home" element={<Home />} />
-        </Route>
+        <Route index element={<Home />} />
       </Route>
     </Routes>
   );
