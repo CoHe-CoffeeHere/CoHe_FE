55efc7c8865fc4b06c4af7b616ebaa04b2b969d4
nyeonseok <aragorn2358@naver.com>
2025-11-18 17:10:00 +0900

    feat: reward 카드 구현

--- src/pages/Reward.tsx
+++ src/pages/Reward.tsx
@@ -1,3 +1,34 @@
+import rewardStyle from "../css/pages/reward.module.css";
+import characterIcon from "../assets/pages/subscribe/character.svg";
 export default function Reward() {
-  return <div>Reward</div>;
+  return (
+    <>
+      <div className={rewardStyle.topTextDiv}>
+        <span className={rewardStyle.topName}>나의 리워드</span>
+        <span className={rewardStyle.topComment}>
+          포인트를 모으고 혜택을 받아요
+        </span>
+        <img
+          className={rewardStyle.characterIcon}
+          src={characterIcon}
+          alt="Character Icon"
+        />
+      </div>
+      <div className={rewardStyle.rewardCard}>
+        <div className={rewardStyle.textDiv}>
+          <span className={rewardStyle.cardTitle}>사용 가능한 포인트</span>
+          <span className={rewardStyle.point}>2,340P</span>
+        </div>
+        <div className={rewardStyle.progressDiv}>
+          <div className={rewardStyle.progressText}>
+            <span>VIP등급까지</span>
+            <span>1,660P 남음</span>
+          </div>
+          <div className={rewardStyle.progressBar}>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
