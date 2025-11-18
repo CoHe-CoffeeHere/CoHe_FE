a430f7c1dc3ed8e673155c1afde1620e13050141
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-11-18 23:30:44 +0900

    feat: 요청 카드 컴포넌트화

---
+++ src/components/Requestcard.tsx
@@ -0,0 +1,30 @@
+import cardStyle from "../css/components/requestCard.module.css";
+import trashIcon from "../assets/pages/request/trash.svg";
+import accidentIcon from "../assets/pages/request/accident.svg";
+import errorIcon from "../assets/pages/request/error.svg";
+import songIcon from "../assets/pages/request/song.svg";
+import arrowIcon from "../assets/pages/request/arrow-right.svg";
+
+type Props = {
+  text1: string;
+  text2: string;
+  icon:
+    | string
+    | typeof trashIcon
+    | typeof accidentIcon
+    | typeof errorIcon
+    | typeof songIcon;
+};
+
+export default function Requestcard({ text1, text2, icon }: Props) {
+  return (
+    <div className={cardStyle.requestCard}>
+      <img src={icon} alt={text1} />
+      <div className={cardStyle.TextDiv}>
+        <span className={cardStyle.Text1}>{text1}</span>
+        <span className={cardStyle.Text2}>{text2}</span>
+      </div>
+      <img src={arrowIcon} alt="화살표 아이콘" className={cardStyle.arrow} />
+    </div>
+  );
+}
