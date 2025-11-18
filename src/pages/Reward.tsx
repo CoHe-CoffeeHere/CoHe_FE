29292643f6f9328f663231017df9a9bed3b02fe6
nyeonseok <aragorn2358@naver.com>
2025-11-18 17:40:57 +0900

    feat: 진행 중 이벤트 Ui구현

--- src/pages/Reward.tsx
+++ src/pages/Reward.tsx
@@ -1,5 +1,8 @@
 import rewardStyle from "../css/pages/reward.module.css";
 import characterIcon from "../assets/pages/subscribe/character.svg";
+import timeIcon from "../assets/pages/reward/timeEvent.svg";
+import testIcon from "../assets/pages/reward/testEvent.svg";
+import americanoIcon from "../assets/pages/reward/americano.svg";
 export default function Reward() {
   return (
     <>
@@ -26,6 +29,44 @@ export default function Reward() {
           </div>
           <div className={rewardStyle.progressBar}>
             <div></div>
+          </div>
+        </div>
+      </div>
+      <div className={rewardStyle.progressEventDiv}>
+        <div className={rewardStyle.bottomTextDiv}>
+          <span className={rewardStyle.bottomName}>진행 중 이벤트</span>
+          <span className={rewardStyle.bottomComment}>
+            참여하고 포인트를 획득하세요
+          </span>
+        </div>
+        <div className={rewardStyle.eventCardDiv}>
+          <div className={rewardStyle.eventCard}>
+            <div className={rewardStyle.eventCardHeader}>
+              <img src={timeIcon} alt="공부 시간 인증" />
+              <div className={rewardStyle.headerTextDiv}>
+                <span className={rewardStyle.headerText1}>
+                  학습 시간 인증 이벤트
+                </span>
+                <span className={rewardStyle.headerText2}>
+                  4시간 이상 체류 시 500P 적립
+                </span>
+              </div>
+            </div>
+            <div className={rewardStyle.eventBtn}>인증하기</div>
+          </div>
+          <div className={rewardStyle.eventCard}>
+            <div className={rewardStyle.eventCardHeader}>
+              <img src={testIcon} alt="수능 이벤트" />
+              <div className={rewardStyle.headerTextDiv}>
+                <span className={rewardStyle.headerText1}>
+                  수능 합격기원 할인
+                </span>
+                <span className={rewardStyle.headerText2}>
+                  11/7 ~ 11/21 모든 음료 20% 할인
+                </span>
+              </div>
+              <span className={rewardStyle.hot}>HOT</span>
+            </div>
           </div>
         </div>
       </div>
